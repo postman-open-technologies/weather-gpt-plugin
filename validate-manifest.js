5691784f0d858035e24f68d7b0a2ad621e663e90
Gbadebo Bello <gbahdeybohbello@gmail.com>
2023-05-11 16:35:28 +0100

    Added manifest file

--- validate-manifest.js
+++ validate-manifest.js
@@ -5,6 +5,6 @@ const yaml = require("yaml");
 
 const { parser } = require('@exodus/schemasafe');
 
-const parse = parser(yaml.parse(fs.readFileSync('./schemas/ai-plugin.json', "utf8")), { includeErrors: true, allErrors: true, mode: 'lax', requireStringValidation: false });
+const parse = parser(yaml.parse(fs.readFileSync('./plugin-schema.json', "utf8")), { includeErrors: true, allErrors: true, mode: 'lax', requireStringValidation: false });
 
 console.log(parse(JSON.stringify(yaml.parse(fs.readFileSync(process.argv[2], "utf8")))));
